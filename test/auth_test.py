92ce11df5e58e6cd13fc3cb8f4b7a369a881d1fa
SkyProTest <skyprotester123@gmail.com>
2023-02-03 18:50:14 +0300

    +allure

--- test/auth_test.py
+++ test/auth_test.py
@@ -1,19 +1,29 @@
+import allure
 from page.AuthPage import AuthPage
 from page.MainPage import MainPage
 
 def auth_test(browser):
     email = "dmitry.eremin@inbox.ru"
+    password = "sky123456"
+    username = "Dmitry"
+    
     auth_page = AuthPage(browser)
     auth_page.go()
-    auth_page.login_as(email, "sky123456")
+    auth_page.login_as(email, password)
 
     main_page = MainPage(browser)
     main_page.open_menu()
     info = main_page.get_account_info()
     
-    assert main_page.get_current_url().endswith("skyeng_user_d/boards")
-    assert info[0] == "Dmitry"
-    assert info[1] == email
+    current_url = main_page.get_current_url()
+    with allure.step("Проверить, что URL " + current_url + "заканчивается на skyeng_user_d/boards"):
+        assert current_url.endswith("skyeng_user_d/boards")
+    
+    with allure.step("Проверить, что указаны данные пользователя"):
+        with allure.step("Имя пользователя должно быть "+username):
+            assert info[0] == username
+        with allure.step("Почта пользователя должна быть "+email):
+            assert info[1] == email
     
     
     
